005887f7f1a72cc797c28f7dbaf7c6444e605240
Francesco de rosa <fderosa067@gmail.com>
2022-10-18 00:29:42 -0300

    detalles

--- Contenedor/Contenedor.js
+++ Contenedor/Contenedor.js
@@ -1,28 +1,77 @@
+
 const fs = require('fs');
-const productos = require('./productos');
+
 
 class Contenedor {
-    constructor (data){
-        this.data = data
-        this.productos = []
-        console.log("Iniciando");
+    constructor (productos) {
+        this.ruta = productos
     }
-    
-   async save(productos){
-    if (productos instanceof productos === false) {
-        console.log('el parametro no es de tipo Product')
-        return
+
+    getAll = async () => {
+        try {
+            const stock = await fs.promises.readFile(this.ruta, 'utf-8')
+            
+            return JSON.parse(stock);
+
+        } catch (error) {
+            await fs.promises.writeFile(this.ruta, JSON.stringify([], null, 1))
+            const stock = await fs.promises.readFile(this.ruta, 'utf-8')
+            
+            return JSON.parse(stock);
+        }
     }
-    Contenedor.count++
-    productos.id = Contenedor.count
-    this.#add(productos)
-    await this.#writeInDisk()
-    return productos.id
-   }
 
+    saveProducto = async producto => {
+        const arrProductos = await this.getAll()
 
+        arrProductos.push(producto);
+
+        try {
+            await fs.promises.writeFile(this.ruta, JSON.stringify(arrProductos, null, 4))
+            return producto.id
+        } catch (error) {
+            throw new Error('No se guardó el producto')
+        }
+    }
+
+    getById = async id => {
+        const arrProductos = await this.getAll()
+        const productoBuscado = arrProductos.find( p => p.id === id);
+
+        return productoBuscado;
+
+    }
+
+    deleteById = async id => {
+        const arrProductos = await this.getAll()
+        const productoBorrado = arrProductos.filter(p => p.id !== id)
+
+        try {
+            await fs.promises.writeFile(this.ruta, JSON.stringify(productoBorrado, null, 1))
+        } catch (error) {
+            throw new Error('No se pudo actualizar', error)
+        }
+    }
+
+    deleteAll = async () => {
+        return await fs.promises.writeFile(this.ruta, JSON.stringify([], null, 2))
+    }
 
 }
+
+const db = new Contenedor('productos.txt')
+
+const test = async () => {    
+    console.log(await db.saveProducto({ name: "Reloj", price: "15.000", photo: "https://res.cloudinary.com/dngbc9awr/image/upload/v1661611420/samples/ecommerce/analog-classic.jpg", id: 1 }))
+    console.log(await db.getAll())
+    console.log(await db.getById(3))
+    console.log(await db.deleteById(1))
+    await db.deleteAll()
+}
+
+test()
+
+module.exports = Contenedor;
 
 
 
