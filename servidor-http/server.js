de6cd573e74b93008b1f8457ec92f0daa940da8d
Francesco de rosa <fderosa067@gmail.com>
2022-11-02 18:56:29 -0300

    server1

--- servidor-http/server.js
+++ servidor-http/server.js
@@ -1,38 +1,4 @@
-/*const express = require('express');
-const fs = require('fs');
-const Contenedor = require('./contenedor');
 
-const app = express();
-const PORT = process.env.PORT || 8080;
-const Contenedor = new Contenedor('productos.txt');
-
-app.listen(8080,()=>console.log("server en puerto 8080"));
-
-
-const server = app.listen(PORT, () => {
-    console.log(`El server esta corriendo en el puerto ${PORT}`);
-});
-
-
-app.get('/productos', async (req, res) => {
-    const productos = await Contenedor.getAll();
-    res.json(productos);
-});
-
-app.get('/productoRandom', async (req, res) => {
-    const products = await Contenedor.getAll();
-    res.json(products[Math.floor(Math.random() * products.length)]);
-});
-
-app.get('/', (req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-type', 'text/html');
-    var html = fs.readFileSync('./index.html');
-    res.write(html);
-    res.end();
-});
-
-*/
  const http = require("http");
 
  const server = http.createServer((request, response)=>{
